d0775aa3d0acb2abe4763e94c9a860231b2cfb6a
Nury Ahsanti <nurida.ahsanti@student.uinsgd.ac.id>
2016-08-04 14:34:15 +0700

    percobaan aplikasi yang sudah di masukkan method dan stemming basis jar

---
+++ ContohKoneksi/src/contohkoneksi/ContohKoneksi.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package contohkoneksi;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author nury
+ */
+
+public class ContohKoneksi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        // TODO code application logic here
+     //   tutorialKoneksi connection = new tutorialKoneksi();  
+     //connection.getKoneksi();
+    }
+}
